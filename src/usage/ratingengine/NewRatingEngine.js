43b4ede7399b82ea363f20eb59f92e5619a1bc96
harish <harish.komma@onebillsoftware.com>
2022-12-13 18:49:43 +0530

    New Rating Engine Modal

--- src/usage/ratingengine/NewRatingEngine.js
+++ src/usage/ratingengine/NewRatingEngine.js
@@ -24,7 +24,7 @@ export default function NewRatingEngine({className, children}) {
                     {children}
                 </button>
 
-            </div>
+        </div>
 
       <Transition appear show={isOpen} as={Fragment}>
         <Dialog as="div" className="relative z-10" onClose={closeModal}>
@@ -41,39 +41,27 @@ export default function NewRatingEngine({className, children}) {
           </Transition.Child>
 
           <div className="fixed inset-0 overflow-y-auto">
-            <div className="flex min-h-full items-center justify-center p-4 text-center">
+            <div className="flex min-h-full items-start justify-end p-4 text-center">
               <Transition.Child
                 as={Fragment}
                 enter="ease-out duration-300"
-                enterFrom="opacity-0 scale-95"
-                enterTo="opacity-100 scale-100"
+                enterFrom="opacity-0 translate-x-96"
+                enterTo="opacity-100 translate-x-0"
                 leave="ease-in duration-200"
-                leaveFrom="opacity-100 scale-100"
-                leaveTo="opacity-0 scale-95"
+                leaveFrom="opacity-100 translate-x-0"
+                leaveTo="opacity-0 translate-x-96"
               >
-                <Dialog.Panel className="w-full max-w-md transform overflow-hidden rounded-2xl bg-white p-6 text-left align-middle shadow-xl transition-all">
-                  <Dialog.Title
-                    as="h3"
-                    className="text-lg font-medium leading-6 text-gray-900"
-                  >
-                   Rating Engine
+                <Dialog.Panel className="w-full min-h-[calc(100vh-2rem)] max-w-md transform overflow-hidden rounded-md bg-white text-left align-middle shadow-xl transition-all">
+                  <Dialog.Title className="py-3 px-4 bg-gray-200  border-gray-300 border-b ">
+                    <h2> OneBill</h2>
                   </Dialog.Title>
-                  <div className="mt-2">
+                  <div className="mt-2 p-6">
                     <p className="text-sm text-gray-500">
                       Your payment has been successfully submitted. We’ve sent
                       you an email with all of the details of your order.
                     </p>
                   </div>
 
-                  <div className="mt-4">
-                    <button
-                      type="button"
-                      className="inline-flex justify-center rounded-md border border-transparent bg-blue-100 px-4 py-2 text-sm font-medium text-blue-900 hover:bg-blue-200 focus:outline-none focus-visible:ring-2 focus-visible:ring-blue-500 focus-visible:ring-offset-2"
-                      onClick={closeModal}
-                    >
-                      Got it, thanks!
-                    </button>
-                  </div>
                 </Dialog.Panel>
               </Transition.Child>
             </div>
