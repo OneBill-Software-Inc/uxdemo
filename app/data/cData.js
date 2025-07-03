f55f6ddae3968bef78c21fc1abcbcd2e1d8bc761
Harish <harish.komma@onebillsoftware.com>
2025-07-03 22:07:10 +0530

    updated links

--- app/data/cData.js
+++ app/data/cData.js
@@ -9,7 +9,7 @@ const cards = [
     title: 'Configuration',
     text: 'Configure your platforms core settings. Learn how to personalize your branding, create custom invoice templates, and define crucial business defaults such as currency and date format.',
     imgUrl: './images/config.png',
-    videoId: 'bimwyj35hhvg',
+    videoId: 'jxvjivvqytjj',
     icons:
     <LuSettings strokeWidth={1.4} color='#343747' size={26}/>,
   },
@@ -18,7 +18,7 @@ const cards = [
     title: 'Product / Pricing',
     text: 'Define your products and pricing. This section helps you configure the exact products and services you offer, set up their pricing, align taxation and accounting settings, and manage promotional offers.',
     imgUrl: './images/product.png',
-    videoId: '1dhwjvjfkfsg',
+    videoId: 'pscofr9lrgch',
     icons:
     <TfiPackage color='#343747' size={26}/>,
   },
@@ -27,7 +27,7 @@ const cards = [
     title: 'Subscribers',
     text: 'Set up and manage your end customers, who are the core of your business. This video shows you how to easily view customer information, respond to queries, and make necessary changes to any subscriber account.',
     imgUrl: './images/oneview.png',
-    videoId: 'qwruvavq1won',
+    videoId: 'iov7sxwwbdkw',
     icons:
     <LuUsers strokeWidth={1.5} color='#343747' size={26}/>,
   },
@@ -37,7 +37,7 @@ const cards = [
     title: 'Purchase Subscriptions',
     text: 'Easily manage new subscriptions for your customers. This video walks you through the entire sales process, from navigating options to completing a successful sale for your end customer.',
     imgUrl: './images/order.png',
-    videoId: 'vdub72u0nh9t',
+    videoId: 'vbp5peqcmhrd',
     icons:
     <TfiShoppingCartFull color='#343747' size={26}/>,
   },
@@ -47,7 +47,7 @@ const cards = [
     title: 'Invoice / AR',
     text: 'Effortlessly manage all your invoicing and payments. This video shows you how to create invoices (from new purchases or existing transactions), process customer payments, and handle any disputes by applying credit or debit adjustments.',
     imgUrl: './images/ar.png',
-    videoId: 'zyjm5gr6wgpp',
+    videoId: 'zmx5fwzdq9pb',
     icons:
     <LuCassetteTape strokeWidth={1.2} color='#343747' size={26}/>,
   },
@@ -57,7 +57,7 @@ const cards = [
     title: 'Reports / Dashboard',
     text: 'Keep a pulse on your business performance. This video shows you how to effectively use our standard dashboards and operational reports to monitor your key KPIs and metrics.',
    imgUrl: './images/reports.png',
-   videoId: 'wsm5nwcdhuwi',
+   videoId: 'vhozxn2dkkiz',
    icons:
     <LuChartNoAxesCombined strokeWidth={1.4} color='#343747' size={26}/>,
   } ,
