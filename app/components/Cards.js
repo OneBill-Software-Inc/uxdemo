9f7ee5ab24553a31074c8e237c190b55abf3fd7a
Harish <harish.komma@onebillsoftware.com>
2025-07-14 12:05:03 +0530

    Video title text change.

--- app/components/Cards.js
+++ app/components/Cards.js
@@ -7,8 +7,7 @@ export default function Cards() {
       <div className="mx-auto max-w-7xl px-6 lg:px-8">
         <div className="pb-6">
           <h1 className="text-pretty text-base pb-6 text-gray-900">
-            Get started in just a few simple steps and unlock the power of
-            seamless billing and revenue management.
+            Watch these videos in sequence to get up and running.
           </h1>
         </div>
         <div className="grid grid-cols-1 gap-8 sm:grid-cols-2 lg:grid-cols-3">
